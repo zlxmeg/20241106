eb6c78a2382b099b05b4c8d5dfe4c76e6998f726
megan <135104319+zlxmeg@users.noreply.github.com>
2024-11-06 16:44:21 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/<name>")
+def greet(name):
+    return "Hello" + name
+
+
+if __name__=="__main__":
+    app.run(debug=True)
